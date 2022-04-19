99deba7a3321f27b70bd3d144726286c25a4c080
Zach Harder <zach.hardder@gmail.com>
2022-04-19 16:32:30 -0500

    seeded dummy data

--- server/db/models/ShoppingCart.js
+++ server/db/models/ShoppingCart.js
@@ -5,7 +5,7 @@ module.exports = db.define("ShoppingCart", {
   name: {
     type: Sequelize.STRING, // default value, this.owner.name shopping cart?
   },
-  userId: {
-    defaultValue: 1234,
-  },
+  // userId: {
+  //   defaultValue: 1234,
+  // },
 });
