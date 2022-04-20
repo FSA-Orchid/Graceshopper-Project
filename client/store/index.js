388a6a41e1b094ed71fc02a0acdcdb32f60d376a
LyChristopherC <LyChristopherC@gmail.com>
2022-04-20 11:36:09 -0400

    Added Frontend Routes

--- client/store/index.js
+++ client/store/index.js
@@ -1,21 +1,20 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import singleProduct from './singleProduct'
-import products from './products'
-
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import singleProduct from './singleProduct';
+import allproducts from './allproducts';
 
 const reducer = combineReducers({
-auth: auth,
-singleProduct: singleProduct,
-products: products
-})
+  auth: auth,
+  singleProduct: singleProduct,
+  products: allproducts,
+});
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default store;
+export * from './auth';
