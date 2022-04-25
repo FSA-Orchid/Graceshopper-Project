35345007e10ceb7478df89886eafe1e753a36111
JRC91 <jimcarmody91@gmail.com>
2022-04-25 14:42:00 -0400

    userPage mostly done

--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -6,6 +6,9 @@ import { setOrderThunk } from '../store/orderHistory';
 import { fetchUserThunk } from '../store/singleUser';
 import { updateUserThunk } from '../store/users';
 
+//Wanted to make the order dates collapsible
+
+
 class UserPage extends React.Component {
 constructor() {
   super()
@@ -13,26 +16,14 @@ constructor() {
     one: {display: 'none'},
     two: {display: 'none'},
   }
+  this.accordionContent =[]
   this.toggler=this.toggler.bind(this)
-  this.innerToggle=this.innerToggle.bind(this)
+  this.accordionToggle = this.accordionToggle.bind(this)
 }
 
 componentDidMount() {
   this.props.fetcherOrders(this.props.auth.id);
   this.props.fetchUser(this.props.auth.id)
-}
-
-innerToggle (order) {
-  let changes = ReactDOM.findDOMNode(UserPage).getElementsByClassName(`contentOrder${order.id}`)
-  console.log(changes)
-      if(changes.style.display === 'none'){
-        changes.style.display === 'block'
-        return
-      }
-      if(changes.style.display === 'block'){
-        changes.style.display === 'none'
-      }
-
 }
 
 toggler (id) {
@@ -47,6 +38,18 @@ if(nestState.display === 'block'){
   this.setState({nestState})
 }}
 
+accordionToggle(key) {
+  let changes = this.accordionContent[key]
+  console.log(changes)
+  if(changes.style.display === 'none'){
+    changes.style.display = 'block'
+    return
+  }
+  else {
+    changes.style.display = 'none'
+  }
+}
+
 
   render (){
 
@@ -57,23 +60,24 @@ if(nestState.display === 'block'){
       <li><button onClick= {() => this.toggler('two')}> Order History</button></li>
     </ul>
     <div className= 'contentUser' style= {{display: this.state.one.display}}>
-    <h2>{this.props.user.userName}</h2>
-    <h3>{this.props.user.address}</h3>
+    <h2>{this.props.auth.username} --- {this.props.auth.email}</h2>
+    <h3>{this.props.auth.address}</h3>
     </div>
 
     <div className = 'contentUser' style= {{display: this.state.two.display}}>
-      {this.props.orders.map((order)=>{
+      {this.props.orders.map((order, key)=>{
       return (
-      <div key = {order.id}>
-      <button type="button" className="collapsible"  onClick = {() => this.innerToggle(order)}>Ordered on {order.updatedAt}</button>
-      <form className={`contentOrder${order.id}`} style={{display: 'none'}}>
-      {order.products.map((product) => {
-        return (<div key={product.id}>
+      <div key = {key}>
+      <button type="button" onClick={() => this.accordionToggle(key)} className="collapsible">Ordered on {order.updatedAt}</button>
+      <form style={{display: 'none'}} ref={accordionContent => this.accordionContent[key] = accordionContent}>
+      {order.products.map((product, key) => {
+        return (<div key={key}>
             <h5>{product.name}</h5>
             <p>{product.make} {product.model} {product.year}</p>
-            <p> {product.price} </p>
+            <p> {(product.price)/100} x {product.orderProduct.inventory} = {(product.orderProduct.totalPrice)/100} </p>
           </div>)
       })}
+      <p>Total: $</p>
       </form>
       </div>)
     })}
@@ -90,7 +94,8 @@ const mapState = (state) => {
   return {
     user: state.user,
     auth: state.auth,
-    orders: state.orders}
+    orders: state.orders
+  }
 }
 const mapDispatch = (dispatch) => {
   return {
